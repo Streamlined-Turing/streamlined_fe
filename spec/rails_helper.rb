878931af70e9a30d7a89aca095784b6662d29c0e
Thomas Casady <tscasady@gmail.com>
2023-02-22 14:04:29 -0600

    Configure shoulda matchers and simplecov.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'spec'
+  add_filter 'channels'
+  add_filter 'jobs'
+  add_filter 'mailers'
+end
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -52,4 +61,13 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end

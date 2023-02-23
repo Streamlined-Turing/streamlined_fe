bea7f394bde60747b553f61ddf192375ed9188d5
Alex Pitzel <pitzelalex@gmail.com>
2023-02-22 22:05:29 -0500

    test: Create initial login test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,8 +4,9 @@ class LoginController < ApplicationController
   def create
     user_data = request.env['omniauth.auth']
     user_json = UserSerializer.serialize(user_data)
-    user = UserFacade.new(user_json)
-    session[:user_id] = user.id
+
+    user_id = UserFacade.login(user_json)
+    session[:user_id] = user_id
     redirect_to root_path
   end
 end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,9 +3,35 @@ require 'rails_helper'
 RSpec.describe 'Login Page', type: :feature do
   describe 'when a user' do
     describe 'visits the login path, it' do
-      it 'should display the app name' do
+      before(:each) do
+        OmniAuth.config.test_mode = true
+      end
+
+      it 'redirect to landing and have session id' do
+        stub_request(:post, 'http://localhost:5000/users')
+        .to_return(status: 201,
+                   body: File.read('spec/fixtures/alex_login_response.json'),
+                   headers: {})
+
+        OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+                                                                             provider: 'google',
+                                                                             uid: '123545',
+                                                                             info: {
+                                                                               email: 'me@me.com',
+                                                                               name: 'Bono'
+                                                                             },
+                                                                             credentials: {
+                                                                               token: '555'
+                                                                             }
+                                                                           })
+
         visit login_path
         expect(page).to have_button 'Continue With Google'
+
+        click_button 'Continue With Google'
+
+        expect(current_path).to eq root_path
+        expect(page).to have_content('User ID: 1')
       end
     end
   end

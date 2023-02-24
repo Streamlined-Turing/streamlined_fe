b879047278fb5bac5f5854df6f0b411425f86735
Alex Pitzel <pitzelalex@gmail.com>
2023-02-24 17:31:17 -0500

    test: Create dashboard basic display test

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -2,19 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'user show page', type: :feature do
   describe 'as a logged in user' do
-    let(:user) { {
-      id: '1',
-      sub: '104505147435508023263',
-      name: 'Alex Pitzel',
-      username: 'pitzelalex',
-      email: 'pitzelalex@gmail.com',
-      picture: 'https://lh3.googleusercontent.com/a/AEdFTp5vj_rzxJzWHjgqM1-InqDI0fJWxwpHK_zElpKLgA=s96-c'
-    } }
+    let(:user) do
+      {
+        id: '1',
+        sub: '104505147435508023263',
+        name: 'Alex Pitzel',
+        username: 'pitzelalex',
+        email: 'pitzelalex@gmail.com',
+        picture: 'https://lh3.googleusercontent.com/a/AEdFTp5vj_rzxJzWHjgqM1-InqDI0fJWxwpHK_zElpKLgA=s96-c'
+      }
+    end
 
     it 'displays details about my account' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit dashboard_path
 
-      expect(page).to have_content
+      expect(page).to have_content(user[:username])
+      expect(page).to have_content(user[:email])
+      expect(page).to have_content(user[:name])
+      expect(page.find('img')[:src]).to eq user[:picture]
     end
 
     it 'has a button to edit my profile'

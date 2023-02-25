457b3d376102f8a690d0f7ce7fd9da728960e7d1
Thomas Casady <tscasady@gmail.com>
2023-02-25 17:59:05 -0600

    Update field names.

--- app/poros/media.rb
+++ app/poros/media.rb
@@ -1,7 +1,31 @@
-class Media 
-  attr_reader :title
+class Media
 
   def initialize(data)
-    @title = data[:title] 
+    @id = data[:id]
+    @title = data[:attributes][:title]
+    @poster = data[:attributes][:poster]
+    @description = data[:attributes][:description]
+    @genre = data[:attributes][:genres]
+    @runtime = data[:attributes][:runtime]
+    @type = data[:attributes][:type]
+    @year = data[:attributes][:release_year]
+    @us_rating = data[:attributes][:us_rating]
+    @trailer = data[:attributes][:trailer]
+    @language = data[:attributes][:language]
+    @streaming_sources = data[:attributes][:sub_services]
+    @audience_score = data[:attributes][:audience_score]
+    # TODO: Temporary, didn't want to write out the attr_readers
+    # until we are sure they won't change.
+    instance_variables.each do |inst_var|
+      self.class.send(:attr_reader, inst_var.to_s[1..].to_sym)
+    end
+  end
+
+  def genres
+    @genre.join(', ')
+  end
+
+  def sources
+    @streaming_sources.join(', ')
   end
 end

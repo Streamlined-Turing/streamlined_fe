092f39a1ad29be7d8429b0b085a8ead8dd41e9ad
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-25 14:05:09 -0800

    test for no more than 15 results returned in search results collection

--- spec/facades/media_facade_spec.rb
+++ spec/facades/media_facade_spec.rb
@@ -2,20 +2,18 @@ require 'rails_helper'
 
 RSpec.describe MediaFacade do 
   describe '.search_results' do 
-    it 'can return a collection of media result objects' do 
+    it 'can return a collection of a maximum of 15 media result objects' do 
       stub_request(:get, "http://localhost:5000/api/v1/media?q=bad")
       .to_return(status: 200, body: File.read('./spec/fixtures/search_movies_response.json'), headers: {})
 
       all_media_results = MediaFacade.search_results('bad')
 
       expect(all_media_results).to be_an Array 
+      expect(all_media_results.count).to be <= 15
+      
       all_media_results.each do |media|
         expect(media).to be_an_instance_of(MediaResult)
       end
-    end
-
-    xit 'will return maximum of 15 results' do 
-      
     end
   end
 end

c8b506415f41204faa7a54d86186f33ee9c9e925
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-26 15:15:26 -0800

    remove title match query no longer needed in facade for frontend

--- app/facades/media_facade.rb
+++ app/facades/media_facade.rb
@@ -2,10 +2,7 @@ class MediaFacade
   
   def self.search_results(media_query)
     data = MediaService.media_search(media_query)
-    results = data[:data].find_all do |media|
-      media[:attributes][:title].downcase.include?(media_query.downcase)
-    end 
-    limit_15 = results.first(15)
+    limit_15 = data[:data].first(15)
     limit_15.map do |media_result_data|
       MediaResult.new(media_result_data)
     end

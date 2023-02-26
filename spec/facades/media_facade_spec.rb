762f2f6efb7080611e95a34dcaa809d9ab850974
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-26 12:12:51 -0800

    update spec to look for search media fixture

--- spec/facades/media_facade_spec.rb
+++ spec/facades/media_facade_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe MediaFacade do
   describe '.search_results' do 
     it 'can return a collection of a maximum of 15 media result objects' do 
       stub_request(:get, "http://localhost:5000/api/v1/media?q=bad")
-      .to_return(status: 200, body: File.read('./spec/fixtures/search_movies_response.json'), headers: {})
+      .to_return(status: 200, body: File.read('./spec/fixtures/search_media_response.json'), headers: {})
 
       all_media_results = MediaFacade.search_results('bad')
 

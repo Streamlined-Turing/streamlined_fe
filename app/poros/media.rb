06cb3b369cf64b111c448b547a32f25355a777e2
Thomas Casady <tscasady@gmail.com>
2023-02-25 18:21:53 -0600

    Tweak media poro fields.

--- app/poros/media.rb
+++ app/poros/media.rb
@@ -1,4 +1,16 @@
 class Media
+  attr_reader :id,
+              :title,
+              :poster,
+              :description,
+              :runtime,
+              :type,
+              :release_year,
+              :rating,
+              :trailer,
+              :language,
+              :audience_score
+
 
   def initialize(data)
     @id = data[:id]
@@ -8,24 +20,19 @@ class Media
     @genre = data[:attributes][:genres]
     @runtime = data[:attributes][:runtime]
     @type = data[:attributes][:type]
-    @year = data[:attributes][:release_year]
-    @us_rating = data[:attributes][:us_rating]
+    @release_year = data[:attributes][:release_year]
+    @rating = data[:attributes][:rating]
     @trailer = data[:attributes][:trailer]
     @language = data[:attributes][:language]
-    @streaming_sources = data[:attributes][:sub_services]
+    @sub_services = data[:attributes][:sub_services]
     @audience_score = data[:attributes][:audience_score]
-    # TODO: Temporary, didn't want to write out the attr_readers
-    # until we are sure they won't change.
-    instance_variables.each do |inst_var|
-      self.class.send(:attr_reader, inst_var.to_s[1..].to_sym)
-    end
   end
 
   def genres
     @genre.join(', ')
   end
 
-  def sources
-    @streaming_sources.join(', ')
+  def sub_services
+    @sub_services.join(', ')
   end
 end

ecd8044a9c32b3e8009addea41422249bc5f6bca
Joe King <j.king92@gmail.com>
2023-02-28 06:41:39 -0700

    refactor: Changed the login controller to a session controller to align closer with RESTful practices

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 
-  delete '/logout', to: 'login#destroy'
-  post '/auth/google_oauth2/callback', to: 'login#create'
-  post '/login', to: 'login#create'
+  delete '/logout', to: 'session#destroy'
+  post '/auth/google_oauth2/callback', to: 'session#create'
+  post '/login', to: 'session#create'
+  get '/onboarding', to: 'session#edit'
+  patch '/onboarding', to: 'session#update'
 
   get '/search', to: 'search#index'
 
   resource :dashboard, except: [:new, :index, :create], controller: 'users'
-
-  get '/onboarding', to: 'login#edit'
-  patch '/onboarding', to: 'login#update'
 
   resources :media, only: [:show]
 end

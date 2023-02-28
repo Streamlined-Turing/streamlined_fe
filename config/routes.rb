aa91eac67e8d862eeda52a20941c468a91c129ca
Joe King <j.king92@gmail.com>
2023-02-28 06:43:54 -0700

    removed comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 
-  delete '/logout', to: 'session#destroy'
   post '/auth/google_oauth2/callback', to: 'session#create'
   post '/login', to: 'session#create'
   get '/onboarding', to: 'session#edit'
   patch '/onboarding', to: 'session#update'
+  delete '/logout', to: 'session#destroy'
 
   get '/search', to: 'search#index'
 

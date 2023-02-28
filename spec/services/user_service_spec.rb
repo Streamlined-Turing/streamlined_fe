306e7c1f14153d05b7687a66fa993ffa88c2af85
Joe King <j.king92@gmail.com>
2023-02-27 18:21:24 -0700

    test: user_service#delete with sad path test

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UserService, :vcr do
+  let(:user_data) do
+    { sub: '104505147435508023263',
+      name: 'Alex Pitzel',
+      email: 'pitzelalex@gmail.com',
+      picture: 'https://lh3.googleusercontent.com/a/AGNmyxZxvaMaqWjnwCfSs2_g9yETZREpYAM5GPNneX2pbw=s96-c' }
+  end
   describe '#login' do
-    let(:user_data) do
-      { sub: '104505147435508023263',
-        name: 'Alex Pitzel',
-        email: 'pitzelalex@gmail.com',
-        picture: 'https://lh3.googleusercontent.com/a/AGNmyxZxvaMaqWjnwCfSs2_g9yETZREpYAM5GPNneX2pbw=s96-c' }
-    end
 
     it 'can get details about a user from the back end' do
       response = UserService.login(user_data)
@@ -51,6 +51,23 @@ RSpec.describe UserService, :vcr do
       expect(response[:data][:attributes][:username]).to eq('pitzelalex')
       expect(response[:data][:attributes][:email]).to eq('pitzelalex@gmail.com')
       expect(response[:data][:attributes][:picture]).to eq('https://lh3.googleusercontent.com/a/AGNmyxZxvaMaqWjnwCfSs2_g9yETZREpYAM5GPNneX2pbw=s96-c')
+    end
+  end
+
+  describe '#delete' do
+    let(:user_id) { '3' }
+
+    it 'deletes the user and returns a status of 204' do
+      login_response = UserService.login(user_data)
+      response = UserService.delete(user_id)
+
+      expect(response.status).to eq(204)
+    end
+
+    it 'returns a 404 if user id does not exist' do
+      response = UserService.delete('100')
+
+      expect(response.status).to eq(404)
     end
   end
 end

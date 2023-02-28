28b21fcc287f0015ae54706b961951a19fd45a68
Joe King <j.king92@gmail.com>
2023-02-28 06:38:46 -0700

    refactor: routes so logout uses a delete verb

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing#index'
 
-  get '/logout', to: 'login#destroy'
+  delete '/logout', to: 'login#destroy'
   post '/auth/google_oauth2/callback', to: 'login#create'
   post '/login', to: 'login#create'
 
   get '/search', to: 'search#index'
-  # get '/dashboard', to: 'users#show'
+
   resource :dashboard, except: [:new, :index, :create], controller: 'users'
-  # get '/dashboard/edit', to: 'users#edit'
-  # patch '/dashboard/edit', to: 'users#update'
-  # delete '/dashboard', to: 'users#destroy'
+
   get '/onboarding', to: 'login#edit'
   patch '/onboarding', to: 'login#update'
 

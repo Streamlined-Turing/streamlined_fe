b4a3a12b10bca54c07a204d145db4ae9444d2c23
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-01 10:52:32 -0800

    add configuration to vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@
 require 'webmock/rspec'
 require 'vcr'
 VCR.configure do |config|
+  config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('<GOOGLE_CLIENT_ID') { ENV['GOOGLE_CLIENT_ID'] }
